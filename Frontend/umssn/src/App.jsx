7c0f09d2e696dee1b245e33042cd0f576c4497f3
Zziwa Raymond Ian <zziwaraymondian@Ziwas-MacBook-Pro.local>
2023-04-15 22:29:12 +0300

    Building demo interfaces

---
+++ Frontend/umssn/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Login from './components/authentication/login'
+
+function App() {
+  return (
+    <div className="App">
+        <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/umssn/src/components/authentication/admin_login_form/admin_login_form.jsx
@@ -0,0 +1,19 @@
+const AdminLogin = () => {
+    return(
+        <>
+            <div className="mb-3">
+                <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="johndoe" style={{ color: "#8CA6FE;" }} />
+                        <label for="floatingInput">Username</label>
+                </div>
+            </div>
+            <div className="mb-3">
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Password</label>                </div>
+            </div>
+        </>
+    )
+}
+
+export default AdminLogin
