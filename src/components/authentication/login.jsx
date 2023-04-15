1bcfc199a040f9a7d9e2fad364325a0945a7a3c1
Zziwa Raymond Ian <zziwaraymondian@Ziwas-MacBook-Pro.local>
2023-04-15 22:37:32 +0300

    Working on demo interfaces

---
+++ src/components/authentication/login.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { Form } from "react-bootstrap";
+import AdminLogin from "./admin_login_form/admin_login_form"
+import TeacherLogin from "./teacher_login_form/teacher_login_form"
+import StudentLogin from "./student_login_form/student_login_form"
+import logo from './../../../public/imgs/badge.png'
+
+const Login = () => {
+    const [role, setRole] = useState('')
+
+    const handleRoleData = event => {
+        event.preventDefault()
+        setRole(event.target.value)
+    }
+    return(
+        <div className="container min-vh-100 d-flex justify-content-center align-items-center">
+            <Form className="login-form">
+                <div className="mb-3">
+                    <img src={logo} alt="logo" height="180px" />
+                    <p style={{ color: "#3452A3", textAlign: "center", fontFamily: "akshar", fontStyle: "normal", fontSize: "22px" }} className="login-header">UMSSN</p>
+                </div>
+                <div className="mb-3">
+                    <select class="form-select" aria-label="Default select example" onChange={handleRoleData}>
+                        <option selected>Role</option>
+                        <option value="admin">Administrator</option>
+                        <option value="teacher">Teacher</option>
+                        <option value="student">Student / Parent</option>
+                    </select>
+                </div>
+                {role === 'admin' && 
+                    <AdminLogin />
+                }
+                {role === 'teacher' && 
+                    <TeacherLogin />
+                }
+                {role === 'student' &&
+                    <StudentLogin />
+                }
+                <div className="mb-3">
+                    <button style={{ width: "100%", border: "none", color: "white", height: "45px", backgroundColor: "#3452A3" }}>LOGIN</button>
+                </div>
+                <div className="mb-3">
+                    <p style={{ textAlign: "center", fontSize: "18px", marginTop: "10px", color: "#3E5AA7" }}>Terms &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy</p>
+                </div>
+            </Form>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/authentication/student_login_form/student_login_form.jsx
@@ -0,0 +1,52 @@
+const StudentLogin = () => {
+    return(
+        <>
+            <div className="mb-3">
+                <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="johndoe" />
+                        <label for="floatingInput">First Name</label>
+                </div>
+            </div>
+            <div className="mb-3">
+                <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="johndoe" />
+                        <label for="floatingInput">Middle Name</label>
+                </div>
+            </div>
+            <div className="mb-3">
+                <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="johndoe"  />
+                        <label for="floatingInput">Last Name</label>
+                </div>
+            </div>
+            <div className="mb-3">
+                <select class="form-select" aria-label="Default select example" >
+                        <option selected>Select Student Class</option>
+                        <option value="s1">Senior One</option>
+                        <option value="s2">Senior Two</option>
+                        <option value="s3">Senior Three</option>
+                        <option value="s4">Senior Four</option>
+                        <option value="s5">Senior Five</option>
+                        <option value="s6">Senior Six</option>
+                </select>
+            </div>
+            <div className="mb-3">
+                <select class="form-select" aria-label="Default select example" >
+                        <option selected>Select Class Stream</option>
+                        <option value="s1n">Senior One North</option>
+                        <option value="s1e">Senior One East</option>
+                        <option value="s1s">Senior One South</option>
+                        <option value="s1w">Senior One West</option>
+                </select>
+            </div>
+            <div className="mb-3">
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"  />
+                    <label for="floatingPassword">Password</label>                
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default StudentLogin
